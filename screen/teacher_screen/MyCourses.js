31228bbfb71cc285c797f8611b809b5f2d4506e7
shible77 <shible0805@gmail.com>
2024-02-24 00:58:05 +0600

    update in Course of teacher view

--- screen/teacher_screen/MyCourses.js
+++ screen/teacher_screen/MyCourses.js
@@ -1,9 +1,11 @@
-import { StyleSheet, Text, View, TouchableOpacity, FlatList, Pressable } from 'react-native'
+import { StyleSheet, Text, View, TouchableOpacity, FlatList, Pressable, TextInput } from 'react-native'
 import React, { useState, useEffect } from 'react'
 import { Feather } from '@expo/vector-icons';
 import CourseModal from '../../components/CourseModal';
 import { firebase } from './../../config';
 import { LinearGradient } from 'expo-linear-gradient';
+import { AntDesign } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
 
 
 
@@ -13,7 +15,7 @@ const MyCourses = () => {
   const [courseName, setCourseName] = useState('');
   const [courseDescription, setCourseDescription] = useState('');
   const [userCourses, setUserCourses] = useState([]);
-  
+  const [searchText, setSearchText] = useState('')
   const openModal = () => {
     setModalVisible(true)
   }
@@ -76,6 +78,10 @@ const MyCourses = () => {
     }
 
   }
+
+  const searchCourse = () => {
+
+  }
   return (
     <View style={styles.mainPage}>
       {isModalVisible ? <CourseModal isModalVisible={isModalVisible} setModalVisible={setModalVisible} addCourse={addCourse}
@@ -87,9 +93,28 @@ const MyCourses = () => {
               <Text style={{ fontSize: 25 }}>Courses</Text>
               <View style={styles.myButton}>
                 <TouchableOpacity onPress={openModal} >
-                  <Text style={styles.buttonText}>Create Course</Text>
+                  <View style={{display: 'flex', flexDirection : 'row'}}>
+                    <Ionicons name="add-circle-outline" size={20} color="white" />
+                    <Text style={styles.buttonText}> Create Course</Text>
+                  </View>
+
                 </TouchableOpacity>
               </View>
+            </View>
+          </View>
+          <View style={styles.search}>
+            <View style={styles.field}>
+              <View style={{ marginVertical: 8, padding: 3, borderRadius: 3, marginRight: 5 }}><AntDesign name="search1" size={24} color="black" /></View>
+              <TextInput
+                style={{ color: 'black', width: '80%', fontSize: 18 }}
+                placeholder='Class 8: Math'
+                cursorColor={'black'}
+                onChangeText={(text) => setSearchText(text)}></TextInput>
+            </View>
+            <View style={styles.myButton2}>
+              <TouchableOpacity onPress={searchCourse} >
+                <Text style={styles.buttonText}>Search</Text>
+              </TouchableOpacity>
             </View>
           </View>
           <View View style={[styles.courseListing, { justifyContent: userCourses.length === 0 ? 'center' : null }, { alignItems: userCourses.length === 0 ? 'center' : null }]} >
@@ -98,11 +123,11 @@ const MyCourses = () => {
                 data={userCourses}
                 keyExtractor={(item) => item.id}
                 renderItem={({ item }) => (
-                  <Pressable  onPress={() => alert('Press Worked')}>
+                  <Pressable onPress={() => alert('Press Worked')}>
                     <LinearGradient start={{ x: 0, y: 0 }} end={{ x: 1, y: 1 }} colors={['slateblue', 'firebrick']} style={styles.flatListContainer}>
                       <Text style={styles.flatListText}>{"Course Title: " + item.title}</Text>
                       <Text style={[styles.flatListText, { fontSize: 13, marginTop: 5 }]}>{"Description: " + item.description}</Text>
-                      <Text style={[styles.flatListText, { fontSize: 13, marginTop: 5 }]}>{'Created at: '+ item.doc.toDate().toLocaleDateString()}</Text>
+                      <Text style={[styles.flatListText, { fontSize: 13, marginTop: 5 }]}>{'Created at: ' + item.doc.toDate().toLocaleDateString()}</Text>
                     </LinearGradient>
                   </Pressable>
                 )}
@@ -125,17 +150,50 @@ const styles = StyleSheet.create({
     marginTop: 50
   },
   myButton: {
-    backgroundColor: '#7B68EE',
+    backgroundColor: 'firebrick',
     border: '2px solid green',
-    borderRadius: 5,
+    borderRadius: 10,
     padding: 4,
-    marginLeft: 114,
-    marginTop: 5,
+    height: 40,
+    marginLeft: 80,
+    width: '40%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  myButton2: {
+    backgroundColor: 'royalblue',
+    border: '2px solid green',
+    borderRadius: 10,
+    padding: 4,
+    height: 50,
+    width: '34%',
+    margin: 9,
+    // marginTop: 9,
+    justifyContent: 'center',
+    alignItems: 'center'
   },
   buttonText: {
     fontSize: 16,
     color: 'white',
 
+  },
+  search: {
+    display: 'flex',
+    flexDirection: 'row',
+    margin: 5,
+    marginLeft: 18
+  },
+  field: {
+    borderWidth: 2,
+    borderColor: 'black',
+    borderRadius: 10,
+    paddingHorizontal: 5,
+    width: '60%',
+    marginVertical: 10,
+    height: 50,
+    fontSize: 15,
+    display: 'flex',
+    flexDirection: 'row',
   },
   courseListing: {
     marginTop: 10,
