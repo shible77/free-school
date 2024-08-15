e14a15ae3d48333dc2d3cc76a6c5414175aef34b
shible77 <shible0805@gmail.com>
2024-08-16 00:17:12 +0600

    "Updated MyBarChart component to calculate average rating, added course title and rating stats description, and displayed remark based on average rating"

--- components/MyBarChart.js
+++ components/MyBarChart.js
@@ -4,7 +4,23 @@ import { BarChart } from "react-native-chart-kit";
 
 const MyBarChart = (props) => {
     const screenWidth = Dimensions.get('window').width;
-    // console.log(props.data)
+    let averageRating = Math.ceil(
+        ((1 * props.data.one_star) +
+            (2 * props.data.two_star) +
+            (3 * props.data.three_star) +
+            (4 * props.data.four_star) +
+            (5 * props.data.five_star)) /
+        (props.data.one_star +
+            props.data.two_star +
+            props.data.three_star +
+            props.data.four_star +
+            props.data.five_star)
+    );
+    if(props.data.one_star===0 && props.data.two_star===0 && props.data.three_star===0 && props.data.four_star===0 && props.data.five_star===0){
+        averageRating = 0
+    }
+    console.log(averageRating)
+    const Remark = ["Not Rated Yet","Unacceptable", "Below Par", "Adequate", "Impressive", "Exceptional"]
     const data = {
         labels: ["One Star", "Two Star", "Three Star", "Four Star", "Five Star"],
         datasets: [
@@ -24,18 +40,28 @@ const MyBarChart = (props) => {
         barPercentage: 1.2,
         useShadowColorFromDataset: false // optional
     };
+
     return (
         <View style={styles.container}>
-            <BarChart
-                //style={graphStyle}
-                data={data}
-                width={screenWidth-10}
-                height={255}
-                yAxisLabel=""
-                chartConfig={chartConfig}
-                verticalLabelRotation={0}
-                showValuesOnTopOfBars={true}
-            />
+            <View style={styles.headingView}>
+                <Text style={styles.heading}>{'Course Title : ' + props.data.course_title}</Text>
+            </View>
+            <View style={styles.details}>
+                <Text style={styles.dText}>The following graph depicts the rating stats of this course. It shows the number of ratings from 1 star to 5 star : </Text>
+            </View>
+            <View style={styles.barChart}>
+                <BarChart
+                    //style={graphStyle}
+                    data={data}
+                    width={screenWidth}
+                    height={255}
+                    yAxisLabel=""
+                    chartConfig={chartConfig}
+                    verticalLabelRotation={0}
+                    showValuesOnTopOfBars={true}
+                />
+            </View>
+            <Text style={styles.remark}>{'Remark, based on average rating : '+ Remark[averageRating]}</Text>
         </View>
     )
 }
@@ -48,5 +74,29 @@ const styles = StyleSheet.create({
         alignSelf: 'center',
         justifyContent: 'center',
         alignItems: 'center'
+    },
+    barChart: {
+        marginTop: 20,
+    },
+    heading: {
+        fontSize: 22,
+        color: 'crimson'
+    },
+    headingView: {
+        width: '100%',
+        alignItems: 'flex-start',
+        marginTop: 15
+    },
+    details: {
+        marginTop: 20,
+        width: '100%'
+    },
+    dText: {
+        fontSize: 16
+    },
+    remark : {
+        fontSize : 15,
+        fontWeight : 'bold',
+        marginTop : 20
     }
 })
